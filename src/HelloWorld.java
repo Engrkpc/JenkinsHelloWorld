561609193d963e0cd773840bf7391bce48826bdd
Caranzo, Kenneth <kenneth.caranzo@accenture.com>
2021-10-25 18:11:22 +0800

    I made changes

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,6 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World and Welcome to Jenkins Training ");
+        System.out.println("Thi is kenneth, I made some Changes ");
     }
 }
